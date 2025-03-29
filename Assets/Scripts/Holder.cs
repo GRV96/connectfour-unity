00171be2d172946fb2bbd68277a38e9a97b7e6e3
Guyllaume Rousseau <guyllaume.rousseau@hotmail.com>
2025-03-29 19:51:50 -0400

    Constants replaced with fields and variables

--- Assets/Scripts/Holder.cs
+++ Assets/Scripts/Holder.cs
@@ -2,14 +2,6 @@ using UnityEngine;
 
 public class Holder : MonoBehaviour
 {
-	private const float _PANEL_THICKNESS = 0.05f;
-	private const float _PANEL_THICKNESS_HALF = _PANEL_THICKNESS / 2;
-	private const float _TOKEN_DIAMETER = 1.0f;
-	private const float _TOKEN_THICKNESS = 0.1f;
-	private const float _COLUMN_WIDTH = _TOKEN_DIAMETER + _PANEL_THICKNESS;
-	private const float _SEPARATOR_SCALE_Z = _TOKEN_THICKNESS + _PANEL_THICKNESS_HALF;
-	private const float _SEPARATOR_SCALE_Z_HALF = _SEPARATOR_SCALE_Z / 2;
-
 	[SerializeField]
 	private int _nbRows;
 
@@ -21,6 +13,15 @@ public class Holder : MonoBehaviour
 
 	[SerializeField]
 	private GameObject _separatorTemplate;
+
+	[SerializeField]
+	private float _panelThickness = 0.05f;
+
+	[SerializeField]
+	private float _tokenDiameter = 1.0f;
+
+	[SerializeField]
+	private float _tokenThickness = 0.1f;
 
 	void Awake()
 	{
@@ -35,21 +36,26 @@ public class Holder : MonoBehaviour
 
 	private void Build()
 	{
-		float backPanelScaleX = _nbColumns * _TOKEN_DIAMETER + (_nbColumns + 1) * _PANEL_THICKNESS;
-		float backPanelScaleY = _nbRows * _TOKEN_DIAMETER + (_nbRows + 1) * _PANEL_THICKNESS;
+		float panelThicknessHalf = _panelThickness / 2;
+		float columnWidth = _tokenDiameter + _panelThickness;
+		float separatorScaleZ = _tokenThickness + panelThicknessHalf;
+		float separatorScaleZHalf = separatorScaleZ / 2;
+
+		float backPanelScaleX = _nbColumns * _tokenDiameter + (_nbColumns + 1) * _panelThickness;
+		float backPanelScaleY = _nbRows * _tokenDiameter + (_nbRows + 1) * _panelThickness;
 
 		Transform backPanelTransform = _backPanel.transform;
-		backPanelTransform.position = new Vector3(backPanelScaleX/2, 0, _PANEL_THICKNESS_HALF);
-		backPanelTransform.localScale = new Vector3(backPanelScaleX, backPanelScaleY, _PANEL_THICKNESS);
+		backPanelTransform.position = new Vector3(backPanelScaleX/2, 0, panelThicknessHalf);
+		backPanelTransform.localScale = new Vector3(backPanelScaleX, backPanelScaleY, _panelThickness);
 
 		int nbSeparators = _nbColumns + 1;
 		for (int i=0; i<nbSeparators; i++)
 		{
 			GameObject separator = Instantiate(_separatorTemplate, transform);
 			Transform separatorTransform = separator.transform;
-			float xPosition = i * _COLUMN_WIDTH + _PANEL_THICKNESS_HALF;
-			separatorTransform.position = new Vector3(xPosition, 0f, -_SEPARATOR_SCALE_Z_HALF);
-			separatorTransform.localScale = new Vector3(_PANEL_THICKNESS, backPanelScaleY, _SEPARATOR_SCALE_Z);
+			float sepPositionX = i * columnWidth + panelThicknessHalf;
+			separatorTransform.position = new Vector3(sepPositionX, 0f, -separatorScaleZHalf);
+			separatorTransform.localScale = new Vector3(_panelThickness, backPanelScaleY, separatorScaleZ);
 		}
 	}
 }
